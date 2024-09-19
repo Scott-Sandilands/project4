09000b02968e4c199233f81beb094b39cd5acb75
Scott Sandilands <sosandilands@gmail.com>
2024-09-19 18:10:56 -0500

    basic outline of vue files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import GradeInput from './components/GradeInput.vue'
+  import GradeList from './components/GradeList.vue'
+  import GradeAverage from './components/GradeAverage.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Grade Manager</h1>
+    <GradeInput></GradeInput>
+    <GradeList></GradeList>
+    <GradeAverage></GradeAverage>
+  </div>
+</template>
